bf6e50d3cc161ec6997e9748f0977a61ea983d6e
klogpok <kloleg@gmail.com>
2019-07-28 06:29:34 +0000

    Fixed edit

--- index.js
+++ index.js
@@ -5,18 +5,19 @@ const homeRoutes = require('./routes/home');
 const postsRoutes = require('./routes/posts');
 const addRoutes = require('./routes/add');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const hbs = exphbs.create({
-  defaultLayout: 'main',
-  extname: 'hbs',
+    defaultLayout: 'main',
+    extname: 'hbs',
 });
 
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
+app.use(require('body-parser').urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 
 app.use('/', homeRoutes);
 app.use('/posts', postsRoutes);
--- models/post.js
+++ models/post.js
@@ -3,76 +3,75 @@ const path = require('path');
 const fs = require('fs');
 
 class Post {
-  constructor(title, text) {
-    this.title = title;
-    this.text = text;
-    this.id = uuid();
-  }
+    constructor(title, text) {
+        this.title = title;
+        this.text = text;
+        this.id = uuid();
+    }
 
-  toJSON() {
-    return {
-      title: this.title,
-      text: this.text,
-      id: this.id,
-    };
-  }
+    toJSON() {
+        return {
+            title: this.title,
+            text: this.text,
+            id: this.id,
+        };
+    }
 
-  static async update(post) {
-    const posts = await Post.getAllPosts();
-    const idx = posts.findIndex(item => post.id === item.id);
-    console.log(idx);
-    posts[idx] = post;
+    static async update(post) {
+        const posts = await Post.getAllPosts();
+        const idx = posts.findIndex(item => post.id === item.id);
+        posts[idx] = post;
 
-    return new Promise((resolve, reject) => {
-      fs.writeFile(
-        path.join(__dirname, '..', 'data', 'posts.json'),
-        JSON.stringify(posts),
-        'utf-8',
-        err => {
-          if (err) reject(err);
-          else {
-            resolve();
-          }
-        }
-      );
-    });
-  }
+        return new Promise((resolve, reject) => {
+            fs.writeFile(
+                path.join(__dirname, '..', 'data', 'posts.json'),
+                JSON.stringify(posts),
+                'utf-8',
+                err => {
+                    if (err) reject(err);
+                    else {
+                        resolve();
+                    }
+                }
+            );
+        });
+    }
 
-  async save() {
-    const posts = await Post.getAllPosts();
-    posts.push(this.toJSON());
+    async save() {
+        const posts = await Post.getAllPosts();
+        posts.push(this.toJSON());
 
-    return new Promise((resolve, reject) => {
-      fs.writeFile(
-        path.join(__dirname, '..', 'data', 'posts.json'),
-        JSON.stringify(posts),
-        'utf-8',
-        err => {
-          if (err) reject(err);
-          else {
-            resolve();
-          }
-        }
-      );
-    });
-  }
+        return new Promise((resolve, reject) => {
+            fs.writeFile(
+                path.join(__dirname, '..', 'data', 'posts.json'),
+                JSON.stringify(posts),
+                'utf-8',
+                err => {
+                    if (err) reject(err);
+                    else {
+                        resolve();
+                    }
+                }
+            );
+        });
+    }
 
-  static async getPostById(id) {
-    const posts = await this.getAllPosts();
+    static async getPostById(id) {
+        const posts = await this.getAllPosts();
 
-    return posts.find(post => post.id === id);
-  }
+        return posts.find(post => post.id === id);
+    }
 
-  static getAllPosts() {
-    return new Promise((resolve, reject) => {
-      fs.readFile(path.join(__dirname, '..', 'data', 'posts.json'), 'utf-8', (err, content) => {
-        if (err) reject(err);
-        else {
-          resolve(JSON.parse(content));
-        }
-      });
-    });
-  }
+    static getAllPosts() {
+        return new Promise((resolve, reject) => {
+            fs.readFile(path.join(__dirname, '..', 'data', 'posts.json'), 'utf-8', (err, content) => {
+                if (err) reject(err);
+                else {
+                    resolve(JSON.parse(content));
+                }
+            });
+        });
+    }
 }
 
 module.exports = Post;
